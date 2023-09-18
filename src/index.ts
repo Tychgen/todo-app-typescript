cb6e487a93f6130f984702bcc8fb58e391a2478d
Tychgen <alikxelefov@gmail.com>
2023-09-18 18:36:59 +0400

    HTML Elements selected

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,7 @@ type NewTask = {
   title: string;
   completed: boolean;
 };
+
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.getElementById('new-task-form') as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
