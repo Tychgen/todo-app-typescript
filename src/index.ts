beb4005b21b8e1b839c9ad8f16853b1043cf151c
Tychgen <alikxelefov@gmail.com>
2023-09-18 18:36:16 +0400

    Added types for Todo tasks.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 import {v4 as uuidV4} from 'uuid'
+
+type Task = {
+  userId: number;
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+type NewTask = {
+  title: string;
+  completed: boolean;
+};
